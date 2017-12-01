338225a083ec6e7cae16d82509e8ceb53d8bf3c4
Gridscape <Gridscape@10.2.29.87>
2017-12-01 03:53:01 -0800

    5th commit

--- src/firstProject/m.java
+++ src/firstProject/m.java
@@ -3,7 +3,7 @@ package firstProject;
 public class m {
 	public static void main(String args[])
 	{
-		System.out.println("first java class...............");
+		System.out.println("first java class......&&&&&.........");
 	}
 
 }
